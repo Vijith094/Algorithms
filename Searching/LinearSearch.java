3de1a692d3627a08133282e30d44126be98300f9
Vijith094 <41792110+Vijith094@users.noreply.github.com>
2021-05-04 09:53:01 +0530

    Update LinearSearch.java

--- Searching/LinearSearch.java
+++ Searching/LinearSearch.java
@@ -1,1 +1,18 @@
-
+public class LinearSearch 
+{
+	public static int[] input = new int[] {23,55, 21, 19, 66, 44};
+	
+	public static void main (String[] args)
+	{
+		int to_find = 155;
+		for (int a: input)
+		{
+			if (a == to_find)
+			{
+				System.out.println("Integer found");
+				return;
+			}
+		}
+		System.out.println("Integer Not found");
+	}
+}
